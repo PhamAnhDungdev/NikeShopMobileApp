bd710c1824adf41a10c6832868854b4e89de51c7
Phạm Anh Dũng <168076412+PhamAnhDungdev@users.noreply.github.com>
2025-07-08 22:55:06 +0700

    Fix mis code

--- NikeShop/app/src/main/java/com/example/nikeshop/ui/activities/SearchActivity.java
+++ NikeShop/app/src/main/java/com/example/nikeshop/ui/activities/SearchActivity.java
@@ -13,7 +13,7 @@ import androidx.core.view.WindowInsetsCompat;
 import androidx.recyclerview.widget.LinearLayoutManager;
 import androidx.recyclerview.widget.RecyclerView;
 
-import com.example.nikeshop.Adapter.SuggestionAdapter;
+import com.example.nikeshop.adapters.SuggestionAdapter;
 import com.example.nikeshop.R;
 
 import java.util.ArrayList;
